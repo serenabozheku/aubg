7bcf4a57055b30fd3eb822bc06b6f2f5e002c581
serena <serena.bozheku@gmail.com>
2019-02-01 16:56:00 +0200

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def my_funtion1():
+    return 42
+
+
+def my_function(varone, vartwo):
+    return varone * vartwo
+    
+
+print(my_funtion1())
+
+print(my_function(3, 7))
+                   
+
---
+++ variable.py
@@ -0,0 +1,10 @@
+my_string_var="Hello World"
+my_string_var2='Hello World'
+my_int_var=1
+my_float_var=3.14
+my_true_var=True
+my_false_var=False
+
+my_int_var1=4
+my_int_var2=my_int_var1
+my_int_varp=my_int_var1*my_int_var2
