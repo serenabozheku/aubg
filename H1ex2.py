0a3ff2d2293293e7e08cf27c70d1749a9d7d31cf
Serena Bozheku <serena.bozheku@gmail.com>
2019-02-07 22:45:45 +0100

    Exercise2

--- H1ex2.py
+++ H1ex2.py
@@ -1,22 +1,22 @@
 print("This is a notebook")
 
 
-inputstring = input("You can start adding entries:  ").split(",")
+inputstring = input("You can start adding entries:  ").split(",")  #Add elements to the list/notebook
 entrylist = list(inputstring)
 
 print("Now you can:")
 print("1.Add entries.")
 print("2.Delete entries")
 
-action = input("Please choose the next action:  ")
+action = input("Please choose the next action:  ")   #the user can choose to add or delete elements
 
-if action == "1":
+if action == "1":                                     #add elements to the list
     inputaddstring = input().split(", ")
     entrylist.extend(inputaddstring)
     print(entrylist)
 
 
-elif action == "2":
+elif action == "2":                         #remove elements from the list
     inputremovestring = input().split(",")
     entrylist.remove(inputremovestring)
     print(entrylist)
